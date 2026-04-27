5fc8ab180dba013e1dd04f8ba1dc50ab9f32fc98
Federico-Parolo <federico.parolo@itiszuccante.edu.it>
2026-04-27 09:46:13 +0200

    appassocoitempi

--- WeatherStation/main.js
+++ WeatherStation/main.js
@@ -1,22 +1,26 @@
-const SERVER_ADDRESS = "192.168.4.44";
+const SERVER_ADDRESS = "192.168.4.44:3000";
 const canvas = document.querySelector("#dashboard");
 const ctx = canvas.getContext("2d");
-const dataPane = document.querySelector("#main-pane");
+const dataPane = document.querySelector("#data-section");
 const getDataButton = document.querySelector("#getData");
 const addressField = document.querySelector("#ip-address");
+const labsField = document.querySelector("#lab-selection");
+let data = [];
+let labs = {};
 ctx.fillRect(0,0,canvas.width,canvas.width);
 
 
 getDataButton.addEventListener("click",(e) => {
-    getData();
-    if (addressField.value == "") {
+    getData(addressField.value);
+    console.log(addressField.value);
+    if (addressField.value === "") {
         addressField.value = SERVER_ADDRESS;
     } 
 });
 
 
 addressField.addEventListener("keydown", (e) => {
-    if (e.key == "Enter") {
+    if (e.key === "Enter") {
         getData(addressField.value);
     }
     if (addressField.value == "") {
@@ -28,26 +32,70 @@ addressField.addEventListener("keydown", (e) => {
 
 async function getData(address) {
     let response;
-    if (address == null || address == undefined || address == "") {
-        console.log("ADDRESS NOT VALID");
-        response = await fetch("http://" + SERVER_ADDRESS + ":3000/_data");
+    if (address == null || address === undefined || address === "") {
+        console.log("ADDRESS NOT VALID OR EMPTY");
+        response = await fetch("http://" + SERVER_ADDRESS + "/data");
     } else {
-    response = await fetch("http://" + address + ":3000/_data");
+    response = await fetch("http://" + address + "/data");
     }
     if (!response.ok) {
         console.log("Errore nella richiesta");
     } else {
+        dataPane.textContent = '';
         response = await response.json();
-        console.log(response);
-        let newData = document.createElement("div");
-        newData.innerHTML = `<div class="data">
-                <span id="temp">Temperature: ${response[0].temperature}°C</span>
-                <span id="hum">Humidity: ${response[0].humidity}</span>
-                <span id="lum">Luminosity: ${response[0].luminosity}</span>
+        // console.log(response);
+        data = response;
+        for (const sample of response) {
+            parseSample(sample);
+            let newData = document.createElement("div");
+            newData.innerHTML = `<div class="data">
+                <span id="temp">Temperature: ${sample.temperature}°C</span>
+                <span id="hum">Humidity: ${sample.humidity}</span>
+                <span id="lum">Luminosity: ${sample.luminosity}</span>
+                <span id="timestamp">${sample.timestamp}</span>
             </div>`;
-        dataPane.appendChild(newData);
+            dataPane.appendChild(newData);
+
+        }
+        createLabsSelections();
         
+        console.log(labs);   
     }
+}
+
+function parseSample(sample) {
+    let lab = sample.position.split("-")[0];
+    if (!(lab in labs)) {
+        labs[lab] = [];
+    }
+    labs[lab].push(sample);
+}
+
+function createLabsSelections() {
+    for (const [lab,samples] of Object.entries(labs)) {
+        let labButton = document.createElement("input");
+        labButton.type = "button";
+        labButton.value = lab.split("-")[0];
+        labButton.addEventListener("click", e => {
+            showLabDetails(samples);
+        });
+        labsField.appendChild(labButton);
+    }
+}
+
+function showLabDetails(samples) {
+    dataPane.textContent = '';
+    for (const sample of samples) {
+            let newData = document.createElement("div");
+            newData.innerHTML = `<div class="data">
+                <span id="temp">Temperature: ${sample.temperature}°C</span>
+                <span id="hum">Humidity: ${sample.humidity}</span>
+                <span id="lum">Luminosity: ${sample.luminosity}</span>
+                <span id="timestamp">${sample.timestamp}</span>
+            </div>`;
+            dataPane.appendChild(newData);
+
+        }
 }
 
 
